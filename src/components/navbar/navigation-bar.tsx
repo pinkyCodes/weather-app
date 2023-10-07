e2b0c3a9320418771e0745f9646052d77cdcc6d3
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-10-07 18:21:48 +0300

    feat: make NavigationBar with offcanvas functionality for smaller screens

--- src/components/navbar/navigation-bar.tsx
+++ src/components/navbar/navigation-bar.tsx
@@ -1,42 +1,35 @@
 import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import Search from '../search/search';
-
+import TimeAndDate from '../time-and-date/time-and-date';
 
 const NavigationBar = () => {
     return (
-        // <Navbar expand="lg" className="bg-body-tertiary">
-        //     <Container>
-        //         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //         <Navbar.Collapse id="basic-navbar-nav">
-        //             <Nav className="me-auto">
-        //                 <Nav.Link href="#home">Home</Nav.Link>
-        //                 <Nav.Link href="#link">Link</Nav.Link>
-        //                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        //                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        //                     <NavDropdown.Item href="#action/3.2">
-        //                         Another action
-        //                     </NavDropdown.Item>
-        //                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        //                     <NavDropdown.Divider />
-        //                     <NavDropdown.Item href="#action/3.4">
-        //                         Separated link
-        //                     </NavDropdown.Item>
-        //                 </NavDropdown>
-        //             </Nav>
-        //         </Navbar.Collapse>
-        //     </Container>
-        // </Navbar>
-        <Navbar sticky="top" expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="home">Sky Symphony</Navbar.Brand>
-                <Search />
-            </Container>
-        </Navbar>
+        <>
+            <Navbar expand='sm' className="bg-body-tertiary mb-3">
+                <Container fluid>
+                    <Navbar.Brand href="#home">Sky Symphony</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='offcanvasNavbar-expand-sm' />
+                    <Navbar.Offcanvas
+                        id='offcanvasNavbar-expand-sm'
+                        aria-labelledby='offcanvasNavbarLabel-expand-sm'
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id='offcanvasNavbarLabel-expand-sm'>
+                                What would you like to do?
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Search />
+                            <TimeAndDate />
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </>
     );
-};
+}
 
 export default NavigationBar;
