ac9e1b17dff0a1ccc0f27a8851d13f8ae0139de8
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-10-07 20:49:26 +0300

    chore: create constants.ts and add url-s and api key

---
+++ src/common/constants.ts
@@ -0,0 +1,5 @@
+export const OPEN_WEATHER_API_KEY: string = 'b2a32958d56e03d46bcec864de9a3ca9';
+
+export const OPEN_WEATHER_GEOCODING_URL: string = 'http://api.openweathermap.org/geo/1.0/direct'
+
+export const LIMIT_OF_LOCATIONS_IN_API_RESPONSE: number = 3;
